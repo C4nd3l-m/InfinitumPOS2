4323811e976cf862ce71f7e900f0b005eae3f662
C4nd3l-m <c4nd3l.m@gmail.com>
2025-05-10 22:41:34 -0300

    data-source-entities

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,6 +1,11 @@
 import { DataSource } from "typeorm";
 import * as dotenv from 'dotenv';
 import { DB_HOST, DB_DROP, DB_PORT, DB_LOGGING, DB_NAME, DB_PASSWORD, DB_SYNC, DB_USER } from "./envs";
+import { Credential } from "../entities/Credentials.entity";
+import { Product } from "../entities/Products.entity";
+import { Sale } from "../entities/Sale.entity";
+import { User } from "../entities/User.entity";
+import { SaleDetail } from "../entities/SaleDetail.entity";
 
 dotenv.config();
 
@@ -16,7 +21,7 @@ export const AppDataSource = new DataSource({
     },
     synchronize: DB_SYNC,
     logging: DB_LOGGING,
-    entities: [__dirname + "/../entities/**/*.{ts,js}"],
+    entities: [Credential, Product, Sale, User, SaleDetail],
     
 
     dropSchema: DB_DROP,
