5ed31e60ac399ed140fc493fb2dd6289e948195b
C4nd3l-m <c4nd3l.m@gmail.com>
2025-05-15 18:31:00 -0300

    fix routes

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -4,8 +4,8 @@ import {getProductsController, postProductController, deleteProductController, e
 const productsRouter: Router = Router();
 
 productsRouter.get("/", (req: Request, res: Response) => getProductsController(req, res))
-productsRouter.post("/", (req: Request, res: Response) => postProductController(req, res))
-productsRouter.delete("/:id", (req: Request, res: Response) => deleteProductController(req, res))
-productsRouter.put("/:id", (req: Request, res: Response) => editProductController(req, res))
+productsRouter.post("/product", (req: Request, res: Response) => postProductController(req, res))
+productsRouter.delete("/delete/:id", (req: Request, res: Response) => deleteProductController(req, res))
+productsRouter.put("/edit/:id", (req: Request, res: Response) => editProductController(req, res))
 
 export default productsRouter
