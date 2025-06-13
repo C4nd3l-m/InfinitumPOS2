389b6fc9168896b991be9d6ab84aa8ea932fdc84
C4nd3l-m <c4nd3l.m@gmail.com>
2025-06-12 22:41:39 -0300

    configuracion de cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
-import express, { Application } from "express"
-import router from "./routes/indexRoutes"
-import cors from "cors" 
+import express, { Application } from "express";
+import router from "./routes/indexRoutes";
+import cors from "cors";
 import morgan from "morgan";
 const server: Application = express();
 
-server.use(express.json())
+server.use(express.json());
 server.use(morgan("dev"));
-server.use(cors())
-server.use("/", router)
+server.use(
+  cors({
+    origin: ["http://localhost:3000", "https://infinitum-pos.vercel.app"],
+    credentials: true,
+  })
+);
+server.use("/", router);
 
 export default server;
